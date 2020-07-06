632b496a42876080e025e5222052ed99c583f48e
Biju Rajan <bijurajan78@gmail.com>
2020-07-06 18:07:19 -0400

    Hooking up calculate price with tax function

--- src/main/python/cart_item.py
+++ src/main/python/cart_item.py
@@ -6,20 +6,25 @@ class CartItem:
         self.price = price
         self.item = item
 
-    def calculate_price(self):
+    def calculate_sales_tax(self):
+        sales_tax = self.calculate_sales_tax_value()
+        return f'{sales_tax:0.2f}'
+
+    def calculate_sales_tax_value(self):
         if self.item.is_exempt and self.item.is_imported:
-            return f'{self.price * 5 / 100:0.2f}'
+            return self.price * 5 / 100
         if self.item.is_exempt:
-            return "0.00"
+            return 0
         if self.item.is_imported:
             basic_sales_tax = self.round_up_to_2_decimals(self.price * 10/100)
             import_tax = self.round_up_to_2_decimals(self.price * 5/100)
-            return f'{(basic_sales_tax + import_tax):0.2f}'
-        return f'{(self.price * 10) / 100 :0.2f}'
+            return basic_sales_tax + import_tax
+        return self.price * 10 / 100
+
+    def calculate_price_with_tax(self):
+        return f'{(self.price + self.calculate_sales_tax_value()):0.2f}'
 
     def round_up_to_2_decimals(self, price):
         return math.ceil(price * 20) / 20
-
-
 
 
--- src/unittest/python/test_cart_item.py
+++ src/unittest/python/test_cart_item.py
@@ -7,23 +7,47 @@ class TestCartItem:
     def test_should_not_add_sales_tax_on_exempt_items(self):
         item = Item(True, False)
         price = 12.49
-        result = CartItem(item, price).calculate_price()
+        result = CartItem(item, price).calculate_sales_tax()
         assert result == "0.00"
 
     def test_should_add_sales_tax_on_non_exempt_items(self):
         item = Item(False, False)
         price = 14.99
-        result = CartItem(item, price).calculate_price()
+        result = CartItem(item, price).calculate_sales_tax()
         assert result == "1.50"
 
     def test_should_add_import_duty_only_on_exempt_items(self):
         item = Item(True, True)
         price = 10.00
-        result = CartItem(item, price).calculate_price()
+        result = CartItem(item, price).calculate_sales_tax()
         assert result == "0.50"
 
     def test_should_add_sales_tax_and_import_duty_on_non_exempt_items(self):
         item = Item(False, True)
         price = 47.50
-        result = CartItem(item, price).calculate_price()
+        result = CartItem(item, price).calculate_sales_tax()
         assert result == "7.15"
+
+    def test_should_display_price_with_no_tax_on_exempt_items(self):
+        item = Item(True, False)
+        price = 12.49
+        result = CartItem(item, price).calculate_price_with_tax()
+        assert result == "12.49"
+
+    def test_should_display_price_with_basic_tax_on_non_exempt_items(self):
+        item = Item(False, False)
+        price = 14.99
+        result = CartItem(item, price).calculate_price_with_tax()
+        assert result == "16.49"
+
+    def test_should_display_price_for_exempt_imported_items(self):
+        item = Item(True, True)
+        price = 10.00
+        result = CartItem(item, price).calculate_price_with_tax()
+        assert result == "10.50"
+
+    def test_should_display_price_for_non_exempt_imported_items(self):
+        item = Item(False, True)
+        price = 47.50
+        result = CartItem(item, price).calculate_price_with_tax()
+        assert result == "54.65"
